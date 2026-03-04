02143a84f2d16354446e1c9a77b730388a7103a6
Alexander <alexridley1995@gmail.com>
2026-03-03 22:09:44 -0500

    fix(userscript): bump version and enable non-localhost host matching

--- komf.meta.js
+++ komf.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Komf
 // @namespace    https://github.com/DMD770/komf-userscript-kavita
-// @version      0.12.3
+// @version      0.12.4
 // @description  Komf userscript
 // @author       snd-r
-// @include      http://localhost:25600/*
-// @include      http://localhost:5000/*
+// @match        *://*:25600/*
+// @match        *://*:5000/*
 // @grant        none
 // @run-at       document-body
 // @updateURL    https://raw.githubusercontent.com/DMD770/komf-userscript-kavita/main/komf.meta.js
